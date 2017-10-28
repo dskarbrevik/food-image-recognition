461d964136999da34d10a29ec228070cba6b12c2
jtpdowns <jtpdowns@Josephs-MBP.fios-router.home>
2017-10-27 23:47:09 -0400

    refactor: convert MFP parser into a more modular form

--- mfp_parser.py
+++ mfp_parser.py
@@ -3,9 +3,8 @@ import pandas as pd
 from glob import glob
 from os.path import join, split
 
-file_list = glob(join('food101', '*'))
-nutri_dict_list = list()
-for file_name in file_list:
+
+def get_nutrition(file_name):
     nutri_dict = dict()
     file_handle = open(file_name, 'r')
     _, food_label = split(file_name)
@@ -15,10 +14,20 @@ for file_name in file_list:
         if 'col-1' in line and 'alorie' in line:
             block_flag = True
         elif block_flag:
-            calorie_count = int(line.split('>')[1].split('<')[0].replace(',', ''))
+            calorie_count = int(
+                    line.split('>')[1]
+                    .split('<')[0]
+                    .replace(',', ''))
             block_flag = False
     nutri_dict['label'] = food_label
     nutri_dict['calories'] = calorie_count
-    nutri_dict_list.append(nutri_dict)
-nutri_df = pd.DataFrame(nutri_dict_list)
-nutri_df.to_csv('nutri.csv', index=False)
+    return nutri_dict
+
+
+if __name__ == '__main__':
+    file_list = glob(join('food101', '*'))
+    nutri_dict_list = list()
+    for file_name in file_list:
+        nutri_dict_list.append(get_nutrition(file_name))
+    nutri_df = pd.DataFrame(nutri_dict_list)
+    nutri_df.to_csv('nutri.csv', index=False)
