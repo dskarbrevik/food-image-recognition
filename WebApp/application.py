a9526fcd16a14e1bed33744f82806b36e677f368
Joseph Downs <Joseph.Downs@jhuapl.edu>
2017-12-07 09:57:45 -0500

    fix: clean code and use io object and pillow

--- WebApp/application.py
+++ WebApp/application.py
@@ -1,39 +1,50 @@
-from flask import Flask, redirect, render_template,request, url_for, send_from_directory
-#from flask.ext.session import Session
-#scientific computing library for saving, reading, and resizing images
-from scipy.misc import imsave, imread, imresize
-#for regular expressions, saves time dealing with string data
+from flask import \
+        Flask, \
+        redirect, \
+        render_template, \
+        request, \
+        send_from_directory, \
+        url_for
+# from flask.ext.session import Session
+from io import BytesIO
+from PIL import Image
+# for regular expressions, saves time dealing with string data
 import re
-#system level operations (like loading files)
+# scientific computing library for saving, reading, and resizing images
+from scipy.misc import imsave, imread, imresize
+# system level operations (like loading files)
 import sys
-#for reading operating system data
+# for reading operating system data
 import os
 from werkzeug.utils import secure_filename
-#from load import *
+# from load import *
 
 from keras.applications.resnet50 import ResNet50
 from keras.preprocessing import image
-from keras.applications.resnet50 import preprocess_input, decode_predictions
+from keras.applications.resnet50 import \
+        decode_predictions, \
+        preprocess_input
 import numpy as np
 
-#global vars for easy reusability
-#global model, graph
-#initialize these variables
-#model, graph = init()
 
 UPLOAD_FOLDER = "{}/uploads".format(os.getcwd())
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_EXTENSIONS = set([
+    'txt',
+    'pdf',
+    'png',
+    'jpg',
+    'jpeg',
+    'gif'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-#app.config.from_object(__name__)
 
-#Session(app)
 
 def allowed_file(filename):
     return '.' in filename and \
     filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -52,19 +63,32 @@ def index():
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(filepath)
-            img = image.load_img(filepath, target_size=(224, 224))
-            x = image.img_to_array(img)
+            # use an io object to avoid saving to disk
+            image_str = BytesIO()
+            image_str.write(file.read())
+            image_str.seek(0)
+            # using pillow for reshape
+            image = Image.open(image_str)
+            image = image.resize((224, 224))
+            # preprocess the image
+            x = np.array(image, dtype=np.float64)
             x = np.expand_dims(x, axis=0)
             x = preprocess_input(x)
+            # run prediction
             model = ResNet50(weights='imagenet')
             preds = model.predict(x)
-            predictions = decode_predictions(preds, top=3)[0]
+            predictions = decode_predictions(
+                    preds,
+                    top=3)[0]
             top_1 = predictions[0][1]
             top_2 = predictions[1][1]
             top_3 = predictions[2][1]
-            return render_template("prediction.html", filename=filename, top_1=top_1, top_2=top_2, top_3=top_3)
+            return render_template(
+                    "prediction.html",
+                    filename=filename,
+                    top_1=top_1,
+                    top_2=top_2,
+                    top_3=top_3)
 
     else:
         return render_template("index.html")
